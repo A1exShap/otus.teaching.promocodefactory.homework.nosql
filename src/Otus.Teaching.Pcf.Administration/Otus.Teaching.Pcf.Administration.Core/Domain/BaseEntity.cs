5aecc71826143e2e0f6439190d2462bf2e1fd1dd
simkaa47 <simkaa7@gmail.com>
2022-09-02 17:48:11 +0300

    Добавлен класс настроек соединения с MongoDb

--- src/Otus.Teaching.Pcf.Administration/Otus.Teaching.Pcf.Administration.Core/Domain/BaseEntity.cs
+++ src/Otus.Teaching.Pcf.Administration/Otus.Teaching.Pcf.Administration.Core/Domain/BaseEntity.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 
 namespace Otus.Teaching.Pcf.Administration.Core.Domain
 {
     public class BaseEntity
     {
+        [BsonRepresentation(BsonType.ObjectId)]
         public Guid Id { get; set; }
     }
 }
---
+++ src/Otus.Teaching.Pcf.Administration/Otus.Teaching.Pcf.Administration.DataAccess/Settings/IMongoSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Otus.Teaching.Pcf.Administration.DataAccess.Settings
+{
+    public interface IMongoSettings
+    {
+        string DbName { get; set; }
+        string ConnectionString { get; set; }
+    }
+}
